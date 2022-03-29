a564f1fb54ae0e1fe25a356f85136ecff7b356e2
mBillan <49795811+mBillan@users.noreply.github.com>
2022-03-29 21:25:52 +0300

    Create snackbar_show.dart

---
+++ fan_cate/lib/widgets/material/basic/snackbar_show.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:fan_cate/flutx/flutx.dart';
+import 'package:fan_cate/theme/app_theme.dart';
+
+ThemeData theme = AppTheme.theme;
+CustomTheme customTheme = AppTheme.customTheme;
+
+void showSimpleSnackBar(BuildContext context, String text) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: Text(
+        text,
+        style: theme.textTheme.subtitle2!
+            .merge(TextStyle(color: theme.colorScheme.onPrimary)),
+      ),
+      backgroundColor: theme.colorScheme.primary,
+    ),
+  );
+}
+
+void showSnackBarWithAction(BuildContext context, String text) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: FxText.sh2(text,
+          color: theme.colorScheme.onPrimary),
+      backgroundColor: theme.colorScheme.primary,
+      action: SnackBarAction(
+        onPressed: () {},
+        label: "Undo",
+        textColor: theme.colorScheme.onPrimary,
+      ),
+    ),
+  );
+}
+
+void showSnackBarWithFloating(BuildContext context, String text) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: FxText.sh2(text,
+          color: theme.colorScheme.onPrimary),
+      backgroundColor: theme.colorScheme.primary,
+      behavior: SnackBarBehavior.floating,
+    ),
+  );
+}
+
+void showSnackBarWithFloatingAction(BuildContext context, String text) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: FxText.sh2(text,
+          color: theme.colorScheme.onPrimary),
+      backgroundColor: theme.colorScheme.primary,
+      action: SnackBarAction(
+        onPressed: () {},
+        label: "Undo",
+        textColor: theme.colorScheme.onPrimary,
+      ),
+      behavior: SnackBarBehavior.floating,
+    ),
+  );
+}
